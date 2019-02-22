853146e369aec58d1b0c03b2612781648a32499f
Philippe Hébert <philippe.heb@gmail.com>
2019-02-22 04:33:34 -0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: "508xE3sVbKCqxBSGXkOR8cWaX",
-  consumer_secret: "nrMxWNyfuNmh8frOTqrIjj50hVmpdYYAwOXpFpFmkJ0HKhL6fO",
-  access_token_key: "924816090982580225-x3j4lMIyo8XJmOkWpDhbzGORPSnWZDH",
-  access_token_secret: "W1jrDJBNUO6vBXhv3EBUxlnY15p7OVA9hQzOS7Ay7x2fK"
-};
